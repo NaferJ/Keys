fcd73870c41afdb9a171a0498fa41d51bbc6f575
NaferJ <naferjml@gmail.com>
2023-06-25 20:12:01 -0500

    Fase 1 y 2 Completa

---
+++ example-app/resources/views/home.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @role('admin')
+                    {{ __('You are logged in!
+                        Administrador') }}
+                    @endrole
+                    
+                    @role('vigilante')
+                    {{ __('You are logged in!
+                        Vigilante') }}
+                    @endrole
+
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+</div>
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+
+
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+</div>
+@endsection
